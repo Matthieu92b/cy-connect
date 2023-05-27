c21f3f464ea2b88f3641c7ea636554a4203a9309
Matthieu92b <133603299+Matthieu92b@users.noreply.github.com>
2023-05-27 17:21:39 +0200

    Update IHM.h

--- IHM.h
+++ IHM.h
@@ -8,26 +8,27 @@
 #include<ctype.h>
  
  
-extern int Inform_value(int valmin, int valmax);
+int Inform_value(int valmin, int valmax);
 
-extern int init_gamers(gamer tab[3] );
+int init_gamers(gamer tab[3] );
 
-extern void  init_Sizetable(int *a, int *b);
+void  init_Sizetable(int *a, int *b);
 
-extern int ColorOfGamer(int num_gamer,gamer tab[3]);
+int ColorOfGamer(int num_gamer,gamer tab[3]);
 
-extern void print_table(int table[HEIGHT_MAX][WIDTH_MAX],int a,int b, gamer tab[3]);
+void print_table(int table[HEIGHT_MAX][WIDTH_MAX],int a,int b, gamer tab[3]);
 
-extern  int pawn_placement(int a, int b, int table[HEIGHT_MAX][WIDTH_MAX],int num_gamer,gamer tab[3]);
+int pawn_placement(int a, int b, int table[HEIGHT_MAX][WIDTH_MAX],int num_gamer,gamer tab[3]);
 
   
-extern int print_Size_area_rotaded(gamer tab[3],int num_gamer);
+int print_Size_area_rotaded(gamer tab[3],int num_gamer);
 
-extern void rotated_Pawn(int area_rotated, int a, int b,int *row_pivot, int *column_pivot, int *sens);
+void rotated_Pawn(int area_rotated, int a, int b,int *row_pivot, int *column_pivot, int *sens);
 
 
-extern bool exist_empty_box(table[HEIGHT_MAX][WIDTH_MAX],int a);
+bool exist_empty_box(table[HEIGHT_MAX][WIDTH_MAX],int a);
      
+
 
 
 
