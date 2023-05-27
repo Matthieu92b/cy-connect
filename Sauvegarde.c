63f9825db868299d3830a985248f21664af7a687
Matthieu92b <133603299+Matthieu92b@users.noreply.github.com>
2023-05-27 17:17:30 +0200

    Update Sauvegarde.c

--- Sauvegarde.c
+++ Sauvegarde.c
@@ -10,57 +10,54 @@
 
 // fonction qui restaure le jeu qui etait dans le fichier ou nous avons sauvegarder la partie avant 
 void restore_game (int *nb_gamers, gamer tab[3],int *num_gamer,int table[HEIGHT_MAX][WIDTH_MAX],int *width,int *height){
-  char chaine[100];
-  FILE* fichier=NULL;
-  fichier= fopen("save.txt", "r");
-  fscanf(fichier,"%d",nb_gamers);
+  char string[100];
+  FILE* file=NULL;
+  file= fopen("save.txt", "r");
+  fscanf(file,"%d",nb_gamers);
   for (int i = 0; i<*nb_gamers; i++){
-    fscanf(fichier,"%s",chaine);
-    for (int j=0;j<strlen(chaine)+1;j++){
-      tab[i].nom[j]=chaine[j];
+    fscanf(file,"%s",string);
+    for (int j=0;j<strlen(string)+1;j++){
+      tab[i].nom[j]=string[j];
     }
-    fscanf(fichier,"%s",chaine);
-    for (int j=0;j<strlen(chaine)+1;j++){
-      tab[i].color[j]=chaine[j];
+    fscanf(file,"%s",string);
+    for (int j=0;j<strlen(string)+1;j++){
+      tab[i].color[j]=string[j];
     }
     tab[i].winner = false;
   }
-  fscanf(fichier,"%d",num_gamer);
-  fscanf(fichier,"%d",width);
-  fscanf(fichier,"%d",height);
+  fscanf(file,"%d",num_gamer);
+  fscanf(file,"%d",width);
+  fscanf(file,"%d",height);
   for (int i = 0; i<*height; i++){
     for (int j = 0; j<*width; j++){
-      fscanf(fichier,"%d",&table[i][j]);
+      fscanf(file,"%d",&table[i][j]);
     }
   }
-  fclose(fichier);
+  fclose(file);
 }
 
 
-// foonction qui permet de sauvegarder la partie en ouvrant un fichier qui va enregistrer la partie commencer 
+// fonction qui permet de sauvegarder la partie en ouvrant un fichier qui va enregistrer la partie commencer 
 void save_game (int nb_gamers, gamer tab[],int num_gamer,int table[HEIGHT_MAX][WIDTH_MAX],int width,int height){
 
 
-  FILE* fichier=NULL;
-  fichier= fopen("save.txt", "wt+");
+  FILE* file=NULL;
+  file= fopen("save.txt", "wt+");
 
-  fprintf(fichier,"%d\n",nb_gamers);
+  fprintf(file,"%d\n",nb_gamers);
   for (int i = 0; i<nb_gamers; i++)
     {
-       fprintf(fichier,"%s\n",tab[i].nom);  
-       fprintf(fichier,"%s\n",tab[i].color);
+       fprintf(file,"%s\n",tab[i].nom);  
+       fprintf(file,"%s\n",tab[i].color);
     }
-    fprintf(fichier,"%d\n",num_gamer);
+    fprintf(file,"%d\n",num_gamer);
   
-    fprintf(fichier,"%d\n%d\n",width, height);
+    fprintf(file,"%d\n%d\n",width, height);
    for (int i = 0; i<height; i++){
       for (int j = 0; j<width; j++){
-       fprintf(fichier,"%d\n",table[i][j]);
+       fprintf(file,"%d\n",table[i][j]);
        }
     }
-    fclose(fichier);
+    fclose(file);
 }
-
-
-
 
