3fbbdc0ef209c22b7a071c296693f42cb1b051e7
Matthieu92b <133603299+Matthieu92b@users.noreply.github.com>
2023-05-27 17:18:19 +0200

    Update Calcul.h

--- Calcul.h
+++ Calcul.h
@@ -9,17 +9,16 @@
 
  
 
-extern  void init_table(int table[HEIGHT_MAX][WIDTH_MAX],int a,int b);
+void init_table(int table[HEIGHT_MAX][WIDTH_MAX],int a,int b);
  
-extern int init_Firstgamer(int nb_gamers);
+int init_Firstgamer(int nb_gamers);
 
-extern void pawn_gravity(int table[HEIGHT_MAX][WIDTH_MAX],int a, int b,int pawn,int num_gamer,gamer tab[3]);
+void pawn_gravity(int table[HEIGHT_MAX][WIDTH_MAX],int a, int b,int pawn,int num_gamer,gamer tab[3]);
 
-extern void gravity_table(int table[HEIGHT_MAX][WIDTH_MAX], int a, int b, gamer tab[3],int num_gamer);
+void gravity_table(int table[HEIGHT_MAX][WIDTH_MAX], int a, int b, gamer tab[3],int num_gamer);
      
-extern int gamer_winner(int table[HEIGHT_MAX][WIDTH_MAX],int a, int b,gamer tab[3]);
+int gamer_winner(int table[HEIGHT_MAX][WIDTH_MAX],int a, int b,gamer tab[3], int nb_gamers);
 
-extern void Nextgamer(int *gamer,int nb_gamers );
+void Nextgamer(int *gamer,int nb_gamers );
     
-  
  
